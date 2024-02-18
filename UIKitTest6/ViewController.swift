42d18a1d78a680693818543b5a831e446e917f21
Miron Kizesov <miron@coperniq.io>
2024-02-18 15:29:28 +0300

    Add bouncing dynamic square

--- UIKitTest6/ViewController.swift
+++ UIKitTest6/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let squareView = UIView()
+    
+    var animator: UIDynamicAnimator!
+    var snapBehavior: UISnapBehavior?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupViews()
     }
+    
+    func setupViews() {
+        squareView.backgroundColor = .systemBlue
+        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        squareView.center = view.center
+        squareView.layer.cornerRadius = 10
 
+        view.addSubview(squareView)
+        squareView.translatesAutoresizingMaskIntoConstraints = false
+        
+        animator = UIDynamicAnimator(referenceView: view)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        animator.removeAllBehaviors()
+        let touch = touches.first!
+        let location = touch.location(in: self.view)
+        
+        snapBehavior = UISnapBehavior(item: squareView, snapTo: location)
+        snapBehavior?.damping = 0.8
+        animator.addBehavior(snapBehavior!)
+    }
 
 }
 
